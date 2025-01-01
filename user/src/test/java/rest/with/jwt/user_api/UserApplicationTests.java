8f7a09bfc3df2ba0e72d972ffbaebca88d107754
PB <180855918+pbrzozow@users.noreply.github.com>
2025-01-01 18:14:54 +0100

    initial commit

---
+++ user/src/test/java/rest/with/jwt/user_api/UserApplicationTests.java
@@ -0,0 +1,13 @@
+package rest.with.jwt.user_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
