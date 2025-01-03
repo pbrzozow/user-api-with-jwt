4f3651a30b6ce2f5af0515e1f9fec1e3491b120f
PB <180855918+pbrzozow@users.noreply.github.com>
2025-01-03 10:06:57 +0100

    remove default exception handler

--- user/src/main/java/rest/with/jwt/user_api/exception/GlobalExceptionHandler.java
+++ user/src/main/java/rest/with/jwt/user_api/exception/GlobalExceptionHandler.java
@@ -2,17 +2,14 @@ package rest.with.jwt.user_api.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<String> handleAllExceptions(Exception ex){
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An error occured: "+ex.getMessage());
-    }
-
+    
     @ExceptionHandler(IncorrectCredentialsException.class)
     public ResponseEntity<String> handleIncorrectCredentials(Exception ex){
         return ResponseEntity.status(HttpStatus.FORBIDDEN).body( ex.getMessage());
